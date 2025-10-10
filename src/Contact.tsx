f491768de2bcaec637a8eb64a0febc800a0471e9
Aayush Acharya <ayushacharya99@gmail.com>
2025-10-10 17:03:11 -0400

    feat: changed contact email

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -5,22 +5,25 @@ import "./Contact.css";
 const Contact: React.FC = () => {
   return (
     <body className="contact-body">
-        <div className="contact-page">
-            <h1>Contact Us</h1>
-            <p>
-                If you have any questions, feel free to reach out to us at the following email addresses:
-            </p>
-            <ul>
-                <li>
-                <strong>Team Email:</strong>{" "}
-                <a href="mailto:climatepub4kg@gmail.com">climatepub4kg@gmail.com</a>
-                </li>
-                <li>
-                <strong>Jo Pan:</strong>{" "}
-                <a href="mailto:huitong.pan@temple.edu">huitong.pan@temple.edu</a>
-                </li>
-            </ul>
-        </div>
+      <div className="contact-page">
+        <h1>Contact Us</h1>
+        <p>
+          If you have any questions, feel free to reach out to us at the
+          following email addresses:
+        </p>
+        <ul>
+          <li>
+            <strong>Team Email:</strong>{" "}
+            <a href="mailto:climatepub4kg@gmail.com">climatepub4kg@gmail.com</a>
+          </li>
+          <li>
+            <strong>Aayush Acharya:</strong>{" "}
+            <a href="mailto:aayush.acharya@temple.edu">
+              aayush.acharya@temple.edu
+            </a>
+          </li>
+        </ul>
+      </div>
     </body>
   );
 };
